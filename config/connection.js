ee2a456476ad372a179b3722f9a4aee47f216224
Mark B <mrbruce9866@gmail.com>
2019-07-25 21:15:22 -0400

    trying heroku again

--- config/connection.js
+++
@@ -1,9 +0,0 @@
-var Sequelize = require('sequelize');
-
-var sequelize = new Sequelize("burgers_db", "root", "root", {
-    host: "localhost",
-    port: 3306,
-    dialect: "mysql",
-  });
-
-module.exports = sequelize;
