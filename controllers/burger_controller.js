8f6406dd4c01a98e9cce2217f1d5aa2e89c18563
Mark B <mrbruce9866@gmail.com>
2019-07-30 18:18:31 -0400

    limited the calls to the database to get the display of the burgers down to one db call

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -6,55 +6,60 @@ var router = express.Router();
 
 router.get('/', function (req, res) {
     db.Burgers.findAll({
-        where: {
-            devoured: false
-        }
-    }).then(function (data1) {
 
-        db.Burgers.findAll({
-            where: {
-                devoured: true
+    }).then(function (data) {
+
+        var nonDevoured = [];
+        var devoured = [];
+
+        data.forEach(burger => {
+            if (burger.devoured) {
+                devoured.push(burger);
+            } else {
+                nonDevoured.push(burger);
             }
-        }).then(function (data2) {
-            res.render('index', {
-                burgers: data1,
-                devoured: data2
-            });
         });
-    })
+
+        res.render('index', {
+            nonDevoured: nonDevoured,
+            devoured: devoured
+        });
+    });
+
 });
 
-router.post('/api/burger/add', function(req, res){
-    db.Burgers.create({burger_name:req.body.name}).then(function (data){
+router.post('/api/burger/add', function (req, res) {
+    db.Burgers.create({
+        burger_name: req.body.name
+    }).then(function (data) {
         res.status(201).end();
     })
 })
 
-router.put('/api/burger/eat', function(req, res){
+router.put('/api/burger/eat', function (req, res) {
 
     var id = req.body.id;
-    
+
     db.Burgers.update({
         devoured: true
-    },
-    {
+    }, {
         where: {
-            id:id
+            id: id
         }
-    }).then(function (data){
+    }).then(function (data) {
         res.status(202).end();
     })
 })
 
-router.delete('/api/burger/remove', function(req, res){
+router.delete('/api/burger/remove', function (req, res) {
 
     var id = req.body.id;
-    
+
     db.Burgers.destroy({
         where: {
-            id:id
+            id: id
         }
-    }).then(function (){
+    }).then(function () {
         res.status(204).end();
     })
 })
